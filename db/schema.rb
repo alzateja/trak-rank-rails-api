81d1e6082a3d05762ddec1ee85588bc91a8b8d4d
alzateja <alzateja@gmail.com>
2017-04-27 01:18:28 -0400

    Setup Album data seeds from csv file
    I created a csv file that will store my album data. I
    also updated the db/seeds file to allow me to parse the data in the
    csv file to populate the "albums" table in my database.  I can run this
    using the rake db:seed command. Possibly will need to reseed database
    once year and image album data is collected for every album.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20170427033912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.integer  "ranking"
+    t.text     "artist"
+    t.text     "album"
+    t.text     "image"
+    t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database
 # with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the
@@ -8,3 +9,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'album_data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  puts row.to_hash
+  album = Album.new
+  album.ranking = row['ranking']
+  album.artist = row['artist']
+  album.album = row['album']
+  album.image = row['image']
+  album.year = row['year']
+  album.save
+  puts "#{album.artist}, #{album.album} saved"
+end
+
+puts "There are now #{Album.count} rows in the albums table"
